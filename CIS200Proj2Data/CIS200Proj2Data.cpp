d0bd1b647948a1c0cf0919999edf1708f2dfd5df
Souad <Souad@DESKTOP-G73DVD4>
2022-04-15 19:07:06 -0400

    finished data with all values

--- CIS200Proj2Data/CIS200Proj2Data.cpp
+++ CIS200Proj2Data/CIS200Proj2Data.cpp
@@ -8,6 +8,7 @@ using namespace std;
 1,700 B jobs
 950 C jobs
 620 D jobs*/
+//DONE! 
 struct job {
 
 	char priority;
@@ -20,7 +21,7 @@ int main()
 {
 	ofstream dataFile;
 	dataFile.open("Data file.txt");
-	job a[100];
+	job a[2300];
 	if (dataFile.fail()) {
 
 		cout << "Error: unable to open file" << endl;
@@ -28,7 +29,7 @@ int main()
 	}
 	//creating data for type a jobs
 	//i think create at array with these values, then adjust the arrival time based on previous arrival time (so like prev arrival + arrival) to get actual arrival tiem
-	for (int i = 0; i < 100; ++i){
+	for (int i = 0; i < 2300; ++i){
 	
 		a[i] = { 'A', 4 + rand() % 3, 1 + rand() % 5 };
 	
@@ -38,7 +39,7 @@ int main()
 
 	// adding arrival time to previous arrival time (cause cumulative)
 	
-	for (int i = 1, j=0; i < 100 && j<99; ++i, ++j) {
+	for (int i = 1, j=0; i < 2300 && j<2299; ++i, ++j) {
 	
 		a[i].arrivalT += a[j].arrivalT;
 
@@ -46,69 +47,69 @@ int main()
 
 	
 
-	job b[100];
+	job b[1700];
 
 	//creating data for type b jobs 
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 1700; ++i) {
 	
 		b[i] = { 'B',6 + rand() % 3 , 2 + rand() % 10};
 	}
-	for (int i = 1, j = 0; i < 100 && j<99; ++i, ++j) {
+	for (int i = 1, j = 0; i < 1700 && j<1699; ++i, ++j) {
 	
 	
 		b[i].arrivalT += b[j].arrivalT;
 	}
 	
 
-	job mergeAB[200];
+	job mergeAB[4000];
 
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 2300; ++i) {
 	
 		mergeAB[i] = a[i];
 
 	}
 
-	for (int i = 100 , k = 0; i < 200, k < 100; i++, k++) {
+	for (int i = 2300 , k = 0; i < 4000, k < 1700; i++, k++) {
 		mergeAB[i] = b[k];
 	}
 
-	job c[100];
+	job c[950];
 	//creating data for type c jobs
 
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 950; ++i) {
 	
 		c[i] = { 'C',8 + rand() % 10, 6 + rand() % 6};
 
 	}
-	for (int i = 1, j = 0; i < 100 && j < 99; ++i, ++j) {
+	for (int i = 1, j = 0; i < 950 && j < 949; ++i, ++j) {
 
 
 		c[i].arrivalT += c[j].arrivalT;
 	}
 
-	job d[100];
+	job d[620];
 	//creating data for type d jobs
 
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 620; ++i) {
 	
 		d[i] = { 'D',14 + rand() % 10, 9 + rand() % 8};
 	}
-	for (int i = 1, j = 0; i < 100 && j < 99; ++i, ++j) {
+	for (int i = 1, j = 0; i < 620 && j < 619; ++i, ++j) {
 
 
 		d[i].arrivalT += d[j].arrivalT;
 	}
 
 	//merging c and d data
-	job mergeCD[200];
+	job mergeCD[1570];
 
-	for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 620; ++i) {
 
 		mergeCD[i] = d[i];
 
 	}
 
-	for (int i = 100, k = 0; i < 200, k < 100; i++, k++) {
+	for (int i = 620, k = 0; i < 1570, k < 950; i++, k++) {
 		mergeCD[i] = c[k];
 	}
 
@@ -116,17 +117,17 @@ int main()
 
 	//finally, combining CD and AB arrays together 
 	//then fully sort after?
-	job fullMerge[400];
-	for (int i = 0; i < 200; ++i) {
+	job fullMerge[5570];
+	for (int i = 0; i < 1570; ++i) {
 
 		fullMerge[i] = mergeCD[i];
 
 	}
-	for (int i = 200, k = 0; i < 400, k < 200; i++, k++) {
+	for (int i = 1570, k = 0; i < 5570, k < 4000; i++, k++) {
 		fullMerge[i] = mergeAB[k];
 	}
 	cout << "FULL ARRAY UNSORTED"<<endl;
-	for (int i = 0; i < 400; ++i) {
+	for (int i = 0; i < 5570; ++i) {
 
 		cout << fullMerge[i].priority << "  " << fullMerge[i].arrivalT << "   " << fullMerge[i].processT << endl;
 
@@ -135,9 +136,9 @@ int main()
 
 
 	//sort ascending order with arrival time
-	for (int i = 0; i < 400; i++) {
+	for (int i = 0; i < 5570; i++) {
 	
-		for (int j = i + 1; j < 400; j++) {
+		for (int j = i + 1; j < 5570; j++) {
 		
 			if (fullMerge[i].arrivalT > fullMerge[j].arrivalT) {
 				
@@ -150,9 +151,9 @@ int main()
 	}
 
 	//sort the "sorted" with priority included
-	for (int i = 0; i < 400; i++) {
+	for (int i = 0; i < 5570; i++) {
 
-		for (int j = i + 1; j < 400; j++) {
+		for (int j = i + 1; j < 5570; j++) {
 
 			if (fullMerge[i].arrivalT == fullMerge[j].arrivalT && fullMerge[i].priority < fullMerge[j].priority) {
 
@@ -165,9 +166,9 @@ int main()
 	}
 
 	cout << "FULL ARRAY SORTED" << endl;
-	for (int i = 0; i < 400; ++i) {
+	for (int i = 0; i < 5570; ++i) {
 
-		cout << fullMerge[i].priority << "  " << fullMerge[i].arrivalT << "   " << fullMerge[i].processT << endl;
+		dataFile << fullMerge[i].priority << "  " << fullMerge[i].arrivalT << "   " << fullMerge[i].processT << endl;
 
 	}
 
