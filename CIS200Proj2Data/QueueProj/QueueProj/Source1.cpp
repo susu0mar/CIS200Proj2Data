9f0251fb395173b5b4915b341b587036ae1c80cc
Souad <Souad@DESKTOP-G73DVD4>
2022-04-18 18:57:42 -0400

    4/18

--- CIS200Proj2Data/QueueProj/QueueProj/Source1.cpp
+++ CIS200Proj2Data/QueueProj/QueueProj/Source1.cpp
@@ -6,70 +6,6 @@
 #include <vector>
 using namespace std;
 //hi
- void enQueue(struct queueNode** head, job ndata ) {
-
-    queueNode* newNode = new queueNode; //creating new node to put data in
-    queueNode* last = *head;  //creating a temp pointer with head address to traverse the list until it reaches end to insert the new node
-
-    newNode->data = ndata; //giving node data
-
-    newNode->next = NULL; //setting node to null since its the last item on list
-
-    //if the list is empty, made head the new node 
-    if (*head == NULL) {
-        *head = newNode;
-        return;
-    }
-
-    //traverse the list until you reach the last node 
-    while (last->next != NULL) {
-        
-        last = last->next;
-
-    }
-
-    //set last nodes next to be our new node :D
-    last->next = newNode;
-    return;
-
-
-}
-
- void dequeue(struct queueNode* head)
-{
-	 if (isEmpty(&head)) 
-		 return;
-	 else {
-		 //check if it is the head 
-		 queueNode* temp = head;
-		 head = head->next;
-		 
-		 delete temp;
-	 }
-
-}
-
-void InsertHead(queueNode** oldHead, job newJob) {
-
-	queueNode* newHead = new queueNode;
-	newHead-> data = newJob;
-	newHead->next = *oldHead;
-	*oldHead = newHead;
-
-}
-
-bool isEmpty(queueNode** head)
-{
-	if (*head == NULL)
-		return true;
-	else
-		return false;
-}
- 
-job peek(queueNode** head) {
-
-	return (*head)-> data;
-}
 
 //TODO: SIZE OF QUEUE
 
@@ -114,7 +50,7 @@ int main()
 
 
 
-	while //(//data still going )
+	//while //(//data still going )
 	//{
 	//insert
 	//if (fullMerge.priorty == 'D')
